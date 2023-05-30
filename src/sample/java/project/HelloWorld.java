df923226fca7128d54671f8f95889c0837b25eff
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-05-30 20:08:25 +0530

    commit 1

--- src/sample/java/project/HelloWorld.java
+++ src/sample/java/project/HelloWorld.java
@@ -2,6 +2,6 @@ package sample.java.project;
 
 class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello World...");  
+     System.out.println("Hello World!");  
     }
 }
